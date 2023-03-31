ced2ee9b570108822b848f39416ae69a48f807f4
Bryan Granger <bryanwgranger@Bryans-MacBook-Pro-2.local>
2023-03-31 06:32:58 -0400

    ready for deployment

--- otpoems/__init__.py
+++ otpoems/__init__.py
@@ -1,12 +1,7 @@
-import os
-import requests
-import json
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
-
 
 db = SQLAlchemy()
 migrate = Migrate()

f23b6ae4ce63aaeb5c86dff21f2a071061423014
Bryan Granger <bryanwgranger@Bryans-MacBook-Pro-2.local>
2023-03-31 16:51:44 -0400

    update config

--- otpoems/config.py
+++ otpoems/config.py
@@ -1,4 +1,7 @@
+import os
 
 class Config(object):
     SECRET_KEY = 'dev'
-    SQLALCHEMY_DATABASE_URI = "postgresql://bryanwgranger@localhost/otpoems"
+    #SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = "postgresql://bryanwgranger@localhost/otpoems"

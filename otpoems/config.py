c1d3c184d037f926f9ff8fbc8aeed2d400f6faf3
Bryan Granger <bryanwgranger@Bryans-MacBook-Pro-2.local>
2023-04-02 21:48:01 -0400

    new database

--- otpoems/config.py
+++ otpoems/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = 'dev'
-    #SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    #SQLALCHEMY_DATABASE_URI = "postgresql://bryanwgranger@localhost/otpoems"
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_HEROKU')
+
